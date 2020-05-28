74f2b897e27d914ef53e7ed81560b7456fdc4a54
Franco Tiveron <franco.tiveron@gmail.com>
2020-05-28 10:49:16 +1000

    Backup

---
+++ Sam.Web.FrontEnd.Blazor/Services/SamDataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sam.Web.FrontEnd.Blazor.Models;
+
+namespace Sam.Web.FrontEnd.Blazor.Services
+{
+    public interface ISamDataService
+    {
+        IEnumerable<Country> Countries { get; }
+    }
+    public class SamDataService : ISamDataService
+    {
+        public static Country[] _Countries = new Country[] { new Country("USA"), new Country("Australia") };
+
+        IEnumerable<Country> ISamDataService.Countries => _Countries;
+    }
+
+}
